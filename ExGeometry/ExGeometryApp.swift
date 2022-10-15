abe8fc81bd616b2512279731f2f053983f7c79c4
jake <palatable7@naver.com>
2022-10-15 13:20:23 +0900

    Initial Commit

---
+++ ExGeometry/ExGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExGeometryApp.swift
+//  ExGeometry
+//
+//  Created by 김종권 on 2022/10/15.
+//
+
+import SwiftUI
+
+@main
+struct ExGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
